4c337e6ca35b5de4f2881a92e3736d778b61bb5e
Emma Laurent <emma.laurent.je@gmail.com>
2019-08-12 18:10:36 +0200

    setup devise + ajout gem pour photos + ajout variables au modele User

--- Gemfile
+++ Gemfile
@@ -15,6 +15,11 @@ gem 'simple_form'
 gem 'uglifier'
 gem 'webpacker'
 
+gem 'devise'
+gem 'dotenv-rails', groups: [:development, :test]
+gem 'cloudinary', '~> 1.9.1'
+gem 'carrierwave', '~> 1.2'
+
 group :development do
   gem 'web-console', '>= 3.3.0'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
-Rails.application.configure do
+  Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.default_url_options = { host: 'https://lord-of-rent.herokuapp.com', port: 3000 }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
