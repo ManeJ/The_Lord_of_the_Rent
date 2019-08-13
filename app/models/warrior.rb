6886dd13cc2ca71b6985895f78e4ad5e1b400797
jaures kouassi <jauresko@gmail.com>
2019-08-13 11:48:50 +0200

    add the validtes for warrior model

--- app/models/warrior.rb
+++ app/models/warrior.rb
@@ -1,4 +1,6 @@
 class Warrior < ApplicationRecord
   mount_uploader :photo, PhotoUploader
   belongs_to :user
+  validates :nickname, :specialty, :race, :weapon, :price, :photo, :address, presence: true
+  validates :nickname, uniqueness: true
 end
