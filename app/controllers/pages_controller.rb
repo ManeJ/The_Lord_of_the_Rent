be654eeb145f6c5ef4f5864cb4e58ca43cd2c6a3
Emma Laurent <emma.laurent.je@gmail.com>
2019-08-14 11:30:35 +0200

    modif des cards et ajout d'un sample

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
   def home
+    @warriors_sample = []
+    5.times do
+      count = Warrior.count
+      random_offset = rand(count)
+      random_warrior = User.offset(random_offset).first
+      @warriors_sample << random_warrior
+    end
+    @warriors_sample
   end
 end
