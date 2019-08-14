d35691dce295c3b96ba2fd36d8ceaf8731e0a84a
Emma Laurent <emma.laurent.je@gmail.com>
2019-08-14 20:55:42 +0200

    2nd fix Algolia Places, sur form booking cette fois

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -5,6 +5,10 @@ const initAutocomplete = () => {
   if (addressInput) {
     places({ container: addressInput });
   }
+  const addressInput2 = document.getElementById('booking_place');
+  if (addressInput2) {
+      places({ container: addressInput2 });
+  }
 };
 
 export { initAutocomplete };
