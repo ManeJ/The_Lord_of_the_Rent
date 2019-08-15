bd156def3ca4ca6c6e197684f787cea06a763567
Emma Laurent <emma.laurent.je@gmail.com>
2019-08-15 18:17:00 +0200

    logique de resa, modif des routes etc

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   resources :warriors do
     resources :bookings
   end
-  get 'owner/bookings', to: 'bookings#display'
+  get 'owner/bookings', to: 'bookings#display_resa'
   patch 'owner/bookings/:id', to: 'bookings#accept'
   get 'owner/warriors', to: 'warriors#owner'
+  get 'owner/bookings/warriors', to: 'bookings#display_bookings', as: :bookings_warriors
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
