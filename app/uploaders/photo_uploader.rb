fc2f5bbaf3d2cfa83125e3427429373c08d59f51
Emma Laurent <emma.laurent.je@gmail.com>
2019-08-15 12:20:32 +0200

    card show booking + fix position footer + reagencement fichiers cards scss

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,3 +1,15 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
+  process eager: true
+
+  process convert: 'jpg'
+
+  version :thumbnail do
+    cloudinary_transformation width: 100, height: 100, crop: :thumb, gravity: :face
+  end
+
+  version :bright_face do
+    cloudinary_transformation effect: "brightness:30", radius: 20,
+      width: 150, height: 150, crop: :thumb, gravity: :face
+  end
 end
