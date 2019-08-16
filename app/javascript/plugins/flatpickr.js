26d7441ed814f0450dbd99e38222099533f41bf9
jaures kouassi <jauresko@gmail.com>
2019-08-16 15:51:04 +0200

    you put directly the start and the end in a form

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,5 +1,11 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/themes/airbnb.css" // A path to the theme CSS
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
-flatpickr(".datepicker", {})
+flatpickr("#booking_start_date", {
+  altInput: true,
+  plugins: [new rangePlugin({ input: "#booking_end_date" })]
+})
+
+
 
